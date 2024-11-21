30a2ba22d67acfacbf8fb5f91227b4c68821300f
Adrian San Miguel <140763353+asanmiguel12@users.noreply.github.com>
2024-11-21 01:13:11 -0800

    workbook-7 workshop

---
+++ workbook-7 workshop.sql
@@ -0,0 +1,30 @@
+SELECT * 
+FROM CarDealershipDatabase.Dealerships;
+
+SELECT Make, Model
+FROM CarDealershipDatabase.Vehicles 
+JOIN CarDealershipDatabase.Dealerships
+WHERE CarDealershipDatabase.Vehicles.Dealership_Id = 1;
+
+SELECT *
+FROM CarDealershipDatabase.Vehicles 
+WHERE VIN = 1212;
+
+SELECT Name
+FROM CarDealershipDatabase.Vehicles
+JOIN CarDealershipDatabase.Dealerships
+ON CarDealershipDatabase.Vehicles.Dealership_Id = CarDealershipDatabase.Dealerships.Dealership_Id
+WHERE VIN = 1212;
+
+SELECT Name 
+FROM CarDealershipDatabase.Dealerships
+JOIN CarDealershipDatabase.Vehicles
+ON CarDealershipDatabase.Vehicles.Dealership_Id = CarDealershipDatabase.Dealerships.Dealership_Id
+WHERE Make = "Audi" AND Model = "A3";
+
+SELECT * 
+FROM CarDealershipDatabase.Sales_Contracts
+WHERE VIN = 1212
+
+
+
